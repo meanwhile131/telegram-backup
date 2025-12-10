3fdcd113fb7399d65dacb2775fcf5f6c0c372636
meanwhile131 <51206659+meanwhile131@users.noreply.github.com>
2025-12-10 18:20:26 +0400

    Remove user interface from main

--- src/main.cpp
+++ src/main.cpp
@@ -54,10 +54,10 @@ auto overloaded(F... f)
 
 namespace td_api = td::td_api;
 
-class TdExample
+class TelegramBackup
 {
 public:
-    TdExample()
+    TelegramBackup()
     {
         td::ClientManager::execute(td_api::make_object<td_api::setLogVerbosityLevel>(1));
         client_manager_ = std::make_unique<td::ClientManager>();
@@ -77,85 +77,16 @@ public:
             {
                 process_response(client_manager_->receive(10));
             }
-            else
+            while (true)
             {
-                std::cout << "Enter action [q] quit [u] check for updates and request results [c] show chats [m <chat_id> "
-                             "<text>] send message [me] show self [l] logout: "
-                          << std::endl;
-                std::string line;
-                std::getline(std::cin, line);
-                std::istringstream ss(line);
-                std::string action;
-                if (!(ss >> action))
+                auto response = client_manager_->receive(0);
+                if (response.object)
                 {
-                    continue;
+                    process_response(std::move(response));
                 }
-                if (action == "q")
+                else
                 {
-                    return;
-                }
-                if (action == "u")
-                {
-                    std::cout << "Checking for updates..." << std::endl;
-                    while (true)
-                    {
-                        auto response = client_manager_->receive(0);
-                        if (response.object)
-                        {
-                            process_response(std::move(response));
-                        }
-                        else
-                        {
-                            break;
-                        }
-                    }
-                }
-                else if (action == "close")
-                {
-                    std::cout << "Closing..." << std::endl;
-                    send_query(td_api::make_object<td_api::close>(), {});
-                }
-                else if (action == "me")
-                {
-                    send_query(td_api::make_object<td_api::getMe>(),
-                               [this](Object object)
-                               { std::cout << to_string(object) << std::endl; });
-                }
-                else if (action == "l")
-                {
-                    std::cout << "Logging out..." << std::endl;
-                    send_query(td_api::make_object<td_api::logOut>(), {});
-                }
-                else if (action == "m")
-                {
-                    std::int64_t chat_id;
-                    ss >> chat_id;
-                    ss.get();
-                    std::string text;
-                    std::getline(ss, text);
-
-                    std::cout << "Sending message to chat " << chat_id << "..." << std::endl;
-                    auto send_message = td_api::make_object<td_api::sendMessage>();
-                    send_message->chat_id_ = chat_id;
-                    auto message_content = td_api::make_object<td_api::inputMessageText>();
-                    message_content->text_ = td_api::make_object<td_api::formattedText>();
-                    message_content->text_->text_ = std::move(text);
-                    send_message->input_message_content_ = std::move(message_content);
-
-                    send_query(std::move(send_message), {});
-                }
-                else if (action == "c")
-                {
-                    std::cout << "Loading chat list..." << std::endl;
-                    send_query(td_api::make_object<td_api::getChats>(nullptr, 20), [this](Object object)
-                               {
-            if (object->get_id() == td_api::error::ID) {
-              return;
-            }
-            auto chats = td::move_tl_object_as<td_api::chats>(object);
-            for (auto chat_id : chats->chat_ids_) {
-              std::cout << "[chat_id:" << chat_id << "] [title:" << chat_title_[chat_id] << "]" << std::endl;
-            } });
+                    break;
                 }
             }
         }
@@ -181,7 +112,7 @@ private:
     void restart()
     {
         client_manager_.reset();
-        *this = TdExample();
+        *this = TelegramBackup();
     }
 
     void send_query(td_api::object_ptr<td_api::Function> f, std::function<void(Object)> handler)
@@ -200,7 +131,6 @@ private:
         {
             return;
         }
-        // std::cout << response.request_id << " " << to_string(response.object) << std::endl;
         if (response.request_id == 0)
         {
             return process_update(std::move(response.object));
@@ -408,6 +338,6 @@ private:
 
 int main()
 {
-    TdExample example;
-    example.loop();
+    TelegramBackup telegram_backup;
+    telegram_backup.loop();
 }
